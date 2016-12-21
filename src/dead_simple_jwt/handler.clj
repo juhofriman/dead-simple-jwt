b316ce9e71f8a04710d209006c6e08ce7402f079
Juho Friman <juho.friman@solita.fi>
2016-12-21 14:25:56 +0200

    Now requires Authorization header
    Authrozation header must now just start with workd Bearer.

--- src/dead_simple_jwt/handler.clj
+++ src/dead_simple_jwt/handler.clj
@@ -4,8 +4,22 @@
             [ring.middleware.defaults :refer [wrap-defaults site-defaults]]))
 
 (defroutes app-routes
-  (GET "/" [] "Hello World")
+  (GET "/" [] "You have access! This is a secret that should be accessible only with valid credentials")
   (route/not-found "Not Found"))
 
+
+(defn has-valid-token
+  [{auth-header "authorization"}]
+  (and auth-header (.startsWith auth-header "Bearer")))
+
+(defn wrap-authentication
+  [handler]
+  (fn [{headers :headers :as req}]
+    (if (has-valid-token headers)
+      (handler req)
+      {:status 404 :body "No valid token" :headers {"Content-type" "text/plain;charset=UTF-8"}})))
+
 (def app
-  (wrap-defaults app-routes site-defaults))
+  (-> app-routes
+      (wrap-defaults site-defaults)
+      (wrap-authentication)))
